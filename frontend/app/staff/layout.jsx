fa03bc4cfc12b1b1c705f06a6a6274de0a60e0db
shamil-2k7 <shamil2k7g@gmail.com>
2026-07-14 09:58:57 +0530

    staff navbar error fixed

--- frontend/app/staff/layout.jsx
+++ frontend/app/staff/layout.jsx
@@ -1,4 +1,5 @@
 
+import StaffSidebar from "@/components/SaffSidebar/SaffSidebar";
 import styles from "./StaffDashboard.module.css";
 
 export default function StaffLayout({ children }) {
@@ -7,7 +8,7 @@ export default function StaffLayout({ children }) {
       <StaffSidebar />
 
       <div className={styles.main}>
-         <
+         
 
         <main className={styles.content}>
           {children}
