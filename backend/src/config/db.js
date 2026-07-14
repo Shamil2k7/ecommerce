0854b0b0d2b63085cce82861730683fbfa7759b1
misriyaa <misriya8784@gmail.com>
2026-07-14 12:32:12 +0530

    Database (mongoose) connected

---
+++ backend/src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`mongodb connected ${connection.connection.host}`);
+  } catch (error) {
+    console.error(` Database connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
