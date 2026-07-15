10678b76da942e065f4964032199ec584babc9ad
shamil-2k7 <shamil2k7g@gmail.com>
2026-07-15 18:07:06 +0530

    db.js comgfig is fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`mongodb connected ${connection.connection.host}`);
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
-    console.error(` Database connection Error: ${error.message}`);
+    console.error(`Database Connection Error: ${error.message}`);
     process.exit(1);
   }
 };
+
+export default connectDB;
