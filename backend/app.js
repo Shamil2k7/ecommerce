c742a43d4395f9641c3e3cdcffbc8f79d6afff45
misriyaa <misriya8784@gmail.com>
2026-07-16 19:39:46 +0530

    dot env config

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import path from "path";
 import { fileURLToPath } from "url";
-import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
