3bc50f2d917ce23440a236300a270774af227d62
anasmuhammed-2k25 <muhammedanas8727@gmail.com>
2026-07-17 11:19:18 +0530

    make a lil change

--- frontend/app/admin/banners/edit/[id]/page.jsx
+++ frontend/app/admin/banners/edit/[id]/page.jsx
@@ -21,8 +21,6 @@ export default function EditBannerPage() {
 
   const [updating, setUpdating] = useState(false);
 
-  // ================= GET BANNER =================
-
   useEffect(() => {
     if (id) {
       getBanner();
@@ -46,8 +44,6 @@ export default function EditBannerPage() {
     }
   };
 
-  // ================= IMAGE CHANGE =================
-
   const handleImage = (e) => {
     const file = e.target.files[0];
 
@@ -65,8 +61,7 @@ export default function EditBannerPage() {
     reader.readAsDataURL(file);
   };
 
-  // ================= UPDATE =================
-
+  
   const handleUpdate = async () => {
     try {
       setUpdating(true);
@@ -89,8 +84,6 @@ export default function EditBannerPage() {
       setUpdating(false);
     }
   };
-
-  // ================= DELETE =================
 
   const handleDelete = async () => {
     if (!window.confirm("Delete this banner?")) return;
