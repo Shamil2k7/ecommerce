d6f416088f6ceb651e6dd9557e764282c03f57c1
Afsal <afsallll005@gmail.com>
2026-07-19 22:36:08 +0530

    cart schema changed

--- backend/src/models/Cart.js
+++ backend/src/models/Cart.js
@@ -61,7 +61,7 @@ const cartItemSchema = new mongoose.Schema(
 const cartSchema = new mongoose.Schema(
   {
     userId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: String,
       ref: "User",
       required: true,
       unique: true,
