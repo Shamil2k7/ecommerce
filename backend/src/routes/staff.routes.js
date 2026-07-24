53c310e4b17cff58fe44d321ea92afb646bef6c5
Sinan027 <sinan202408@gmail.com>
2026-07-24 10:52:14 +0530

    protect staff routes with authentication and admin authorization

--- backend/src/routes/staff.routes.js
+++ backend/src/routes/staff.routes.js
@@ -3,6 +3,9 @@ import express from "express";
 import upload from "../middlewares/upload.middleware.js";
 import { createStaff, deleteStaff, getAllStaff, getSingleStaff, updateStaff } from "../controllers/staff/staff.controller.js";
 
+import protect from "../middlewares/auth.middleware.js";
+import { isAdmin } from "../middlewares/role.middleware.js";
+
 // import {
   
 //   getAllStaff,
@@ -13,14 +16,14 @@ import { createStaff, deleteStaff, getAllStaff, getSingleStaff, updateStaff } fr
 
 const router = express.Router();
 
-router.post("/", upload.single("image"), createStaff);
+router.post("/",protect,isAdmin, upload.single("image"), createStaff);
 
-router.get("/", getAllStaff);
+router.get("/",protect,isAdmin, getAllStaff);
 
-router.get("/:id", getSingleStaff);
+router.get("/:id",protect,isAdmin, getSingleStaff);
 
-router.put("/:id", upload.single("image"), updateStaff);
+router.put("/:id",protect,isAdmin, upload.single("image"), updateStaff);
 
-router.delete("/:id", deleteStaff);
+router.delete("/:id", protect,isAdmin, deleteStaff);
 
 export default router;
